351d7ac4b4f4d93fd1f1dd5f232182f2e92709b8
Nathan Turner <nathan@appliedimagination.com>
2018-06-14 11:05:09 -0400

    cleanup to app and add package.json

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 require('dotenv').load();
 const Gdax = require('gdax');
+const fs = require('file-system');
 const key = process.env.GDAX_KEY; 
 const b64secret = process.env.GDAX_SECRET; 
 const passphrase = process.env.GDAX_PASS; 
 const apiURI = 'https://api.gdax.com';
 const sandboxURI = 'https://api-public.sandbox.gdax.com';
-const minTimes = 5;
+const minTimes = 3;
 const authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, apiURI);
 
 const publicClient = new Gdax.PublicClient();
 const websocket = new Gdax.WebsocketClient(['BTC-USD', 'ETH-USD', 'LTC-USD']);
 var defaults = new Array();
- defaults['BTC-USD'] = { currency: 'BTC', sell_change: '.09', buy_change: '.1', start_amt: 12000, buy_amt: 50, sell_amt: 50, last_purchased: 15500, last_sold: 15779, min_val: 14000, doing_transaction: false };
- defaults['ETH-USD'] = { currency: 'ETH', sell_change: '.09', buy_change: '.09', start_amt: 450, buy_amt: 50, sell_amt: 50, last_purchased: 650, last_sold: 422, min_val: 400, doing_transaction: false  };
- defaults['LTC-USD'] = { currency: 'LTC', sell_change: '.09', buy_change: '.09', start_amt: 110, buy_amt: 50, sell_amt: 50, last_purchased: 390, last_sold: 150, min_val: 200, doing_transaction: false  };
+//update defaults for change amounts, buy amounts, others if no buy/sell history exists
+ defaults['BTC-USD'] = { currency: 'BTC', sell_change: '.05', buy_change: '.05', start_amt: 6000, buy_amt: 50, sell_amt: 50, last_purchased: 6000, last_sold: 6000, min_val: 6000, doing_transaction: false, sold_last: 0 };
+ defaults['ETH-USD'] = { currency: 'ETH', sell_change: '.05', buy_change: '.05', start_amt: 400, buy_amt: 50, sell_amt: 50, last_purchased: 400, last_sold: 400, min_val: 400, doing_transaction: false, sold_last: 0  };
+ defaults['LTC-USD'] = { currency: 'LTC', sell_change: '.05', buy_change: '.05', start_amt: 100, buy_amt: 50, sell_amt: 50, last_purchased: 100, last_sold: 100, min_val: 100, doing_transaction: false, sold_last: 0  };
 var averages = new Array();
  averages['BTC-USD'] = { count: 0, price: 0 };
  averages['ETH-USD'] = { count: 0, price: 0 };
@@ -28,19 +30,16 @@ launchSocket();
 
 function launchSocket()
 { 
-getAccounts(function(accounts){
-	getAccountOrders(function(orderdata){
-		websocket.on('message', data => { 
-			// work with data 
-//if(data.product_id	=='ETH-USD')		
-	//console.log(data);			
-			analyzeData(data,accounts,orderdata);
-			
-		});  
+	getAccounts(function(accounts){
+		getAccountOrders(function(orderdata){
+			websocket.on('message', data => { 					
+				analyzeData(data,accounts,orderdata);
+				
+			});  
+		});
 	});
-});
-websocket.on('error', err => { /* handle error */ console.log(err); });
-websocket.on('close', () => { /* ... */ console.log('connection closed'); launchSocket(); }); 
+	websocket.on('error', err => { /* handle error */ console.log(err); });
+	websocket.on('close', () => { /* ... */ console.log('connection closed'); launchSocket(); }); 
 }
 
 function analyzeData(feeddata,accounts,orderdata)
@@ -60,18 +59,14 @@ function analyzeData(feeddata,accounts,orderdata)
 		averages[feeddata.product_id].price = feeddata.price;
 		
 		let deciding_price = defaults[feeddata.product_id].last_purchased;
-		if(defaults[feeddata.product_id].last_purchased < defaults[feeddata.product_id].last_sold)
+		if(parseFloat(defaults[feeddata.product_id].last_purchased) < parseFloat(defaults[feeddata.product_id].last_sold) && defaults[feeddata.product_id].sold_last)
 			deciding_price = defaults[feeddata.product_id].last_sold;
 		let sell_price = parseFloat(parseFloat(deciding_price) + parseFloat(deciding_price * defaults[feeddata.product_id].sell_change));
 		let buy_price = parseFloat(deciding_price - parseFloat(deciding_price * defaults[feeddata.product_id].buy_change));
 		
-		
-		if(/* feeddata.side=='sell'  &&*/ feeddata.price > sell_price && averages[feeddata.product_id].count > minTimes && !defaults[feeddata.product_id].doing_transaction  && feeddata.reason!='canceled' ){
-			//sell
-			console.log(feeddata.product_id+' - '+defaults[feeddata.product_id].last_purchased+' '+deciding_price+' '+sell_price+' '+buy_price+' '+feeddata.price);
-			console.log('sell '+feeddata.product_id+' for '+feeddata.price);
-			console.log(currency_accounts[defaults[feeddata.product_id].currency]);
-			//console.log(accounts);
+	
+		if( feeddata.price > sell_price && averages[feeddata.product_id].count > minTimes && !defaults[feeddata.product_id].doing_transaction  && feeddata.reason!='canceled' ){
+			//sell			
 			if(feeddata.price >= trending[feeddata.product_id]){
 				trending[feeddata.product_id].up += 1;
 			}
@@ -81,29 +76,21 @@ function analyzeData(feeddata,accounts,orderdata)
 			console.log(defaults[feeddata.product_id].doing_transaction);
 			trending[feeddata.product_id].price = feeddata.price;
 			averages[feeddata.product_id].count = 0;
-			averages[feeddata.product_id].price = 0;
-			//defaults[feeddata.product_id].doing_transaction = true;
-			transactCoin(feeddata.product_id, feeddata.price, 'sell', accounts, (data) => { if(data) { defaults[feeddata.product_id].doing_transaction = false; } /*accounts = data;/* console.log(data); */ });
+			averages[feeddata.product_id].price = 0;			
+			transactCoin(feeddata.product_id, feeddata.price, 'sell', accounts, (data) => { if(data) { defaults[feeddata.product_id].doing_transaction = false; } });
 		}
-		if(/* feeddata.side=='sell' &&*/ feeddata.price < buy_price && averages[feeddata.product_id].count > minTimes && !defaults[feeddata.product_id].doing_transaction  && feeddata.reason!='canceled' ){
-			//buy
-			console.log(feeddata.product_id+' - '+defaults[feeddata.product_id].last_purchased+' '+deciding_price+' '+sell_price+' '+buy_price);
-			console.log('buy '+feeddata.product_id+' for '+feeddata.price);
-			console.log(currency_accounts[defaults[feeddata.product_id].currency]);
-			//console.log(accounts);
-			console.log(defaults[feeddata.product_id].doing_transaction);
-			//defaults[feeddata.product_id].doing_transaction = true;
+		if( feeddata.price < buy_price && averages[feeddata.product_id].count > minTimes && !defaults[feeddata.product_id].doing_transaction  && feeddata.reason!='canceled' ){
+			//buy			
+			fs.appendFile('log.txt', 'buy '+feeddata.product_id+' for '+feeddata.price+'\r\n', function(err) {});			
 			averages[feeddata.product_id].count = 0;
-			averages[feeddata.product_id].price = 0;
-			
-			transactCoin(feeddata.product_id, feeddata.price, 'buy', accounts, (data) => { if(data) { defaults[feeddata.product_id].doing_transaction = false; }/*accounts = data;/* console.log(data); */ });
+			averages[feeddata.product_id].price = 0;			
+			transactCoin(feeddata.product_id, feeddata.price, 'buy', accounts, (data) => { if(data) { defaults[feeddata.product_id].doing_transaction = false; } });
 		}
 		
 	}		
 }
 
 //api functions
-
 
 //get account info
 function getAccounts(cb){
@@ -148,12 +135,7 @@ function transactCoin(product_id, price, type, accounts, cb){
 	  'product_id': product_id //'BTC-USD',
 	};	
 	defaults[product_id].doing_transaction = true;
-	//console.log('sell '+product_id+' for '+price+'size '+accounts[defaults[product_id].currency].balance);
-	var self = this;
-	//self.accounts = accounts;
-	//accounts[defaults[product_id].currency].balance -= 1;
-	//console.log(accounts);
-	//console.log(currency_accounts);
+	
 	if(price > 0){		
 		if(type=='buy')
 		{			
@@ -167,13 +149,14 @@ function transactCoin(product_id, price, type, accounts, cb){
 				let size = buy_amt / price;
 				size = parseFloat(size);
 				size = size.toFixed(8);
-				//console.log('buy '+product_id+' for '+price);
+				
 				if(size > .001)
 				{
 					coinParams.size = size;
 					console.log(type);
 					console.log(coinParams);
-					 /* authedClient.buy(coinParams, function(err, data){ 
+					fs.appendFile('log.txt', type+' '+JSON.stringify(coinParams)+'\r\n', function(err) {});
+					/* authedClient.buy(coinParams, function(err, data){ 
 						if(err){
 							console.log(err);
 						}
@@ -183,11 +166,10 @@ function transactCoin(product_id, price, type, accounts, cb){
 							defaults[product_id].last_purchased = price;
 							//accounts['USD'].balance -= buy_amt;
 							if(cb)
-								cb(data);
+								getAccounts(function(data){ getAccountOrders(function(orderdata){ return cb(data); })});
 						}
-					});  */
-					//defaults[product_id].last_purchased = price;
-					//accounts['USD'].balance -= buy_amt;
+					});   */
+					
 					getAccounts(function(data){ getAccountOrders(function(orderdata){ return cb(data); })});
 					
 				}
@@ -206,15 +188,13 @@ function transactCoin(product_id, price, type, accounts, cb){
 				if(currency_accounts[defaults[product_id].currency].balance < size)
 					size = currency_accounts[defaults[product_id].currency].balance;
 				size = parseFloat(size);
-				size = size.toFixed(8);
-				//console.log('sell '+product_id+' for '+price+'size'+size);
+				size = size.toFixed(8);				
 				sell_amt = parseFloat(size) * parseFloat(price);
 				if(size >= .001)
 				{
-					coinParams.size = size;
-					console.log(type);
-	console.log(coinParams);
-					/*  authedClient.sell(coinParams, function(err, data){ 
+					coinParams.size = size;					
+					fs.appendFile('log.txt', type+' '+JSON.stringify(coinParams)+'\r\n', function(err) {});
+					/* authedClient.sell(coinParams, function(err, data){ 
 						if(err){
 							console.log(err);
 						}
@@ -226,9 +206,8 @@ function transactCoin(product_id, price, type, accounts, cb){
 							if(cb)
 								getAccounts(function(data){ getAccountOrders(function(orderdata){ return cb(data); })});
 						}
-					});  */
-					//defaults[product_id].last_sold = price;
-					//accounts[defaults[product_id].currency].balance += sell_amt;
+					});   */
+					
 					getAccounts(function(data){ getAccountOrders(function(orderdata){ return cb(data); })});
 				}
 			}
@@ -244,42 +223,55 @@ function transactCoin(product_id, price, type, accounts, cb){
 //get orders
 function getAccountOrders(cb)
 {	
-	authedClient.getFills(function(err, response, data){		
-		 var fills = data; 
-		 //console.log(fills);
-		fills.sort(function(a,b){		  
-		  return new Date(b.created_at) - new Date(a.created_at);
-		});
-		//console.log(fills);
-		
-			let prices = [];
-			prices['BTC-USD'] = { last_purchased: 0, last_sold: 0, added: 0 };
-			prices['ETH-USD'] = { last_purchased: 0, last_sold: 0, added: 0  };
-			prices['LTC-USD'] = { last_purchased: 0, last_sold: 0, added: 0  };
+	try{
+		authedClient.getFills(function(err, response, data){		
+			var fills = data; 
 			
-			for(var key in fills){				
-				let row = fills[key];						
-				if(row.price > 0 && row.side=='buy' /* && prices[row.product_id].last_purchased==0 */ && row.settled==true && prices[row.product_id].added==0)
-				{					
-					prices[row.product_id].last_purchased = row.price;
-					defaults[row.product_id].last_purchased = row.price;
-					prices[row.product_id].added = 1;
-				}
-				if(row.price > 0 && row.side=='sell' /* && prices[row.product_id].last_sold==0 */ && row.settled==true && prices[row.product_id].added==0)
-				{				
-					prices[row.product_id].last_sold = row.price;
-					defaults[row.product_id].last_sold = row.price;
-					prices[row.product_id].added = 1;
-				}
+			if(isArray(fills) && fills.length>0)
+			{
+				fills.sort(function(a,b){		  
+				  return new Date(b.created_at) - new Date(a.created_at);
+				});
 			}			
-			cb = cb || null;
-			if(cb)
-				cb(fills);   
+			
+				let prices = [];
+				prices['BTC-USD'] = { last_purchased: 0, last_sold: 0, sold_added: 0, buy_added: 0 };
+				prices['ETH-USD'] = { last_purchased: 0, last_sold: 0, sold_added: 0, buy_added: 0  };
+				prices['LTC-USD'] = { last_purchased: 0, last_sold: 0, sold_added: 0, buy_added: 0  };
 				
-	});
+				for(var key in fills){				
+					let row = fills[key];						
+					
+					if(parseFloat(row.price) > 0 && row.side=='buy' /* && prices[row.product_id].last_purchased==0 */  && row.settled==true  && prices[row.product_id].buy_added==0 )
+					{									
+						prices[row.product_id].last_purchased = row.price;
+						defaults[row.product_id].last_purchased = row.price;
+						prices[row.product_id].buy_added = 1;
+						defaults[row.product_id].sold_last = 0;
+					}
+					if(parseFloat(row.price) > 0 && row.side=='sell' /* && prices[row.product_id].last_sold==0 */ && row.settled==true && prices[row.product_id].sold_added==0)
+					{				
+						prices[row.product_id].last_sold = row.price;
+						defaults[row.product_id].last_sold = row.price;
+						prices[row.product_id].sold_added = 1;
+						defaults[row.product_id].sold_last = 1;
+					}
+				}			
+				cb = cb || null;
+				if(cb)
+					cb(fills);   
+					
+		});
+	} catch(e){
+		console.log(e);
+		launchSocket();
+	}
 }
 
 
+function isArray (value) {
+	return value && typeof value === 'object' && value.constructor === Array;
+};
 
 
 
@@ -287,14 +279,12 @@ function getAccountOrders(cb)
 /*  not currently used
 function getAccountHistory(type, cb)
 {	
-	getAccounts(function(accounts){
-		//console.log(accounts[type].number);
+	getAccounts(function(accounts){		
 		var acct_id = accounts[type].number;
 		authedClient.getAccountHistory(acct_id, function(err, data){
 			console.log(data.body);
 			var hist = JSON.parse(data.body);
-			for(var key in hist){
-				//console.log(key + ":" + hist[key]);
+			for(var key in hist){				
 				let row = hist[key];
 				console.log(row);
 			}
@@ -305,6 +295,5 @@ function getAccountHistory(type, cb)
 	});
 }
 
-getAccountHistory('LTC', function(data){
-	
+getAccountHistory('LTC', function(data){	
 }); */
